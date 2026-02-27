e3cc73a6fc5230519c16c0d76597baaa6c65bba1
mieraf <elaman99619@gmail.com>
2026-02-27 13:14:17 +0300

    add cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app.routers import auth as auth_router, todo as todo_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 app.include_router(auth_router.router)
 app.include_router(todo_router.router)
